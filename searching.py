ea3a10a8f0fbc898a0b02a077b8ad4dca1294d6d
Martina Khunová <khunovamartina2@gmail.com>
2022-04-12 09:36:25 +0200

    hotovo

--- searching.py
+++ searching.py
@@ -10,16 +10,34 @@ def read_data(file_name, field):  # (nazev souboru ze ktereho vytahujeme, konkre
     file_path = os.path.join(cwd_path, file_name)
     with open(file_path) as f:
         data = json.load(f)
-    # osetreni (jestli je field v keys)
+    # osetreni (jestli je field v keys - aby fce vratila None pro vstupy, ktere nejsou v souboru):
     if field in data.keys():
         return data[field]
     else:
         return None
 
 
+def linear_search(seq, num):
+    positions = []
+    count = 0
+
+    for idx, val in enumerate(seq):
+        if val == num:
+            positions.append(val)
+            count = count + 1
+
+    dict_out = {
+        "positions": positions,
+        "count": count
+    }
+    return dict_out
+
+
 def main():
     unordered_numbers = read_data('sequential.json', 'unordered_numbers')
     print(unordered_numbers)
+    dict_out = linear_search(unordered_numbers, 3)
+    print(dict_out)
 
 
 if __name__ == '__main__':
