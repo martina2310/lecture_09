b4fc2d3f4be3bd94db91e9e5b29b241d99dcdd3d
Martina Khunová <khunovamartina2@gmail.com>
2022-04-12 10:12:51 +0200

    done

--- searching.py
+++ searching.py
@@ -31,13 +31,50 @@ def linear_search(seq, num):
         "count": count
     }
     return dict_out
+# O(n)
+
+def pattern_search(seq, pattern):
+    positions = set()
+    pattern_len = len(pattern)
+
+    left_index = 0
+    rigth_index = pattern_len
+
+    while rigth_index < len(seq):
+        tmp = True
+        for idx_pattern in range(pattern_len):
+            if pattern[idx_pattern] != seq[left_index + idx_pattern]:
+                tmp = False
+                break
+
+        if tmp == True:
+            positions.add(left_index + pattern_len // 2)
+
+        left_index = left_index + 1
+        rigth_index = rigth_index + 1
+
+
+    return positions
+
+# 1 + 1 + 1 + 1 + (n-m) + (n-m) + (n-m)m + (n-m)m + (n-m)m + (n-m) + (n-m) + 1 =
+# 5 + 5(n-m) + 3m(n-m) = m(n-m) =
+# O(mn - m^2) - horsi je mn =>
+# O(mn)
+# n = len(seq), m = len(pattern)
+# nemusi se rozepisovat, jen se podivat, kolikrat bezi nejvnitrnejsi cyklus
 
 
 def main():
     unordered_numbers = read_data('sequential.json', 'unordered_numbers')
     print(unordered_numbers)
+
     dict_out = linear_search(unordered_numbers, 3)
     print(dict_out)
+
+    dna_sequence = read_data('sequential.json', 'dna_sequence')
+    positions = pattern_search(dna_sequence, "ATA")
+    print(positions)
+
 
 
 if __name__ == '__main__':
