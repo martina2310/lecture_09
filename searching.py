1a667282c6cc7928670c6631a538e8e8a6bd367a
Martina Khunová <khunovamartina2@gmail.com>
2022-04-12 09:14:40 +0200

    ahoj

--- searching.py
+++ searching.py
@@ -1,21 +1,25 @@
 import os
+import json
 
-# get current working directory path
+# get current working directory path (musi byt import os)
 cwd_path = os.getcwd()
 
 
-def read_data(file_name, field):
-    """
-    Reads json file and returns sequential data.
-    :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
-    :return: (list, string),
-    """
+def read_data(file_name, field):  # (nazev souboru ze ktereho vytahujeme, konkretni ?promenna? kterou vytahujeme)
+
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as f:
+        data = json.load(f)
+    # osetreni (jestli je field v keys)
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
 
 
 def main():
-    pass
+    unordered_numbers = read_data('sequential.json', 'unordered_numbers')
+    print(unordered_numbers)
 
 
 if __name__ == '__main__':
